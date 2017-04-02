b20806f0bb0610a7aea5a62cc3f723d4467d4059
mohseenrm <mohseenmukaddam6@gmail.com>
2017-04-02 13:15:23 -0700

    update

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-var http = require('http'),
+const http = require('http'),
 	url = require('url'),
 	fs = require('fs'),
 	path = require('path'),
 	argv = require('yargs').argv;
 
+const pool = require(path.join(__dirname, 'db', 'poolHandler'));
+
 //set connection with postgres
 //based on post request handle, generate JSON and emit to client
 //convert json
-
+// console.log(`path: ${__dirname}`);
 var server = http.createServer(function (request, response) {
 	var relpath = url.parse(request.url).pathname;
 
@@ -29,6 +31,20 @@ var server = http.createServer(function (request, response) {
 					response.write(data, 'utf8');
 					response.end();
 				}
+			});
+			break;
+		case 'query':
+			pool.connect((err, client, done) => {
+				if(err)
+					return(console.log(`Error fethching client from pool ${err}`));
+				
+				client.query(
+					"SELECT countryname, year, gdp_local, gdp_us, reserves, externaldept, fdi, trade, inflation, marketcapitalization, centralgovdept, gini, povertygap, employmentratio, populationgrowth, internationalmigrant, fsi FROM public.pivot_indicators LIMIT 1",
+					['1'], (err, result) => {
+						if(err)
+							console.log(`error running query ${err}`);
+						console.log(result);
+					});
 			});
 			break;
 		default:
