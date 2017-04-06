e879a9b2be72939e2ed952d734d138f7a31fe131
mohseenrm <mohseenmukaddam6@gmail.com>
2017-04-05 19:52:56 -0700

    heroku setup

--- db/poolHandler.js
+++ db/poolHandler.js
@@ -1,13 +1,24 @@
 const pg = require('pg');
 
+// const config = {
+// 	user: 'postgres',
+// 	database: 'FSIdb',
+// 	password: '1234',
+// 	host: 'localhost',
+// 	port: 5432,
+// 	max: 10,
+// 	idleTimeoutMillis: 30000
+// };
+
 const config = {
-	user: 'postgres',
-	database: 'FSIdb',
-	password: '1234',
-	host: 'localhost',
+	user: 'teywlppooyogvw',
+	database: 'da2i95mcv6cnrp',
+	password: '02cd6ecd7637fc669f225fde73e6cc69a610ff77d7cceffc657ff3c68a0232a9',
+	host: 'ec2-23-21-219-105.compute-1.amazonaws.com',
 	port: 5432,
 	max: 10,
-	idleTimeoutMillis: 30000
+	idleTimeoutMillis: 30000,
+	ssl: true
 };
 //test
 const pool = new pg.Pool(config);
