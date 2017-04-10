78e1f1a233f5b6e77a0d2bf571d74c30fa487907
mohseenrm <mohseenmukaddam6@gmail.com>
2017-04-10 16:55:52 -0700

    adding packages

--- index2.js
+++ index2.js
@@ -1,7 +1,11 @@
 const argv = require( 'yargs' ).argv,
 	cors = require( 'cors' ),
 	express = require( 'express' ),
-	bodyParser = require( 'body-parser' );
+	bodyParser = require( 'body-parser' ),
+	path = require( 'path' );
+
+const pool = require( path.join( __dirname, 'db', 'poolHandler' ) ),
+	colorHandler = require( path.join( __dirname, 'db', 'colorHandler' ) );
 
 let app = express();
 
@@ -16,7 +20,29 @@ app.get( '/', ( request, response ) => {
 
 app.post( '/main', ( request, response ) => {
 	console.log( request.body );
-	response.send( 'hola' );
+
+	pool.connect((err, client, done) => {
+		if(err)
+			return(console.log(`Error fethching client from pool ${err}`));
+		
+
+		for( i = 1970; i < 2015; i++ ){
+			client.query(
+				"select countryname, year, inflation from pivot_indicators  where year = $1::int order by inflation", [i], (err, result) => {
+					done(err);
+
+					if(err)
+						console.log(`error running query ${err}`);
+
+					console.log(result.rows[0]);
+					colorHandler.updateColors( result.rows );
+				});
+		}
+	});
+
+	response.json( {
+		country: 'XXX'
+	} );
 } );
 
 app.listen( app.get( 'port' ), () => {
