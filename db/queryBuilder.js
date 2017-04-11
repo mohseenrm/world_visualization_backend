1458d031d5032dd61e8a259e4450f398df8df61b
mohseenrm <mohseenmukaddam6@gmail.com>
2017-04-10 18:06:41 -0700

    green tests

--- db/queryBuilder.js
+++ db/queryBuilder.js
@@ -1,5 +1,28 @@
 const queryBuilder = ( filters ) => {
-	return '';
+	return `select countryname, ${getColumn( filters.filter )}, countrycode, ${getColumn( filters.filter )}_color from pivot_indicators where year = ${filters.year} order by ${getColumn( filters.filter )}`;
 };
+
+const getColumn = ( field ) => {
+	switch( field ){
+		case 'GDP':
+			return 'gdp_us';
+		case 'FSI':
+			return 'fsi';
+		case 'INF':
+			return 'inflation';
+		case 'POP':
+			return 'populationgrowth';
+		case 'EMP':
+			return 'employmentratio';
+		case 'RES':
+			return 'reserves';
+		case 'FDI':
+			return 'fdi';
+		case 'TRA':
+			return 'trade';
+		default:
+			return 'fsi';
+	};
+}
 
 module.exports = queryBuilder;
