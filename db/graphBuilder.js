b83030e5ab18738515c6c13bced143432c9c9377
mohseenrm <mohseenmukaddam6@gmail.com>
2017-04-26 20:51:25 -0700

    graph builder added

---
+++ db/graphBuilder.js
@@ -0,0 +1,28 @@
+const graphBuilder = ( filters ) => {
+	return `select ${ getColumn( filters.filter ) } ,year from pivot_indicators where year >= ${ parseInt( filters.year ) - 5 } and year <= ${ parseInt( filters.year ) + 5 } and countrycode = ${ filters.countryc } order by year`;
+};
+
+const getColumn = ( field ) => {
+	switch( field ){
+		case 'GDP':
+			return 'gdp_us';
+		case 'FSI':
+			return 'fsi';
+		case 'INF':
+			return 'inflation';
+		case 'POP':
+			return 'populationgrowth';
+		case 'EMP':
+			return 'employmentratio';
+		case 'RES':
+			return 'reserves';
+		case 'FDI':
+			return 'fdi';
+		case 'TRA':
+			return 'trade';
+		default:
+			return 'fsi';
+	};
+}
+
+module.exports = graphBuilder;
--- index2.js
+++ index2.js
@@ -6,7 +6,8 @@ const argv = require( 'yargs' ).argv,
 
 const pool = require( path.join( __dirname, 'db', 'poolHandler' ) ),
 	colorHandler = require( path.join( __dirname, 'db', 'colorHandler' ) ),
-	queryBuilder = require( path.join( __dirname, 'db', 'queryBuilder' ) );
+	queryBuilder = require( path.join( __dirname, 'db', 'queryBuilder' ) ),
+	graphBuilder = require( path.join( __dirname, 'db', 'graphBuilder' ) );
 
 let app = express();
 
@@ -22,22 +23,44 @@ app.get( '/', ( request, response ) => {
 app.post( '/main', ( request, response ) => {
 	console.log( request.body );
 	//if valid json
-	pool.connect((err, client, done) => {
-		if(err)
-			return(console.log(`Error fethching client from pool ${err}`));
-		client.query( queryBuilder( request.body ), (err, result) => {
-				done(err);
+	pool.connect(( err, client, done ) => {
+		if( err )
+			return( console.log( `Error fethching client from pool ${err}` ) );
+		client.query( queryBuilder( request.body ), ( err, result ) => {
+				done( err );
 
-				if(err)
-					console.log(`error running query ${err}`);
+				if( err )
+					console.log( `error running query ${err}` );
 
-				console.log(result.rows);
+				console.log( result.rows );
 				// colorHandler.updateColors( result.rows );
 				response.json( {
 					data: result.rows
 				} );
 		} );
 	});
+} );
+
+app.post( '/graph', ( request, response ) => {
+	console.log( 'Processing Graph Request....' );
+	console.log( request.body );
+
+	pool.connect(( err, client, done ) => {
+		if( err )
+			return( console.log( `Error fethching client from pool ${err}` ) );
+		client.query( graphBuilder( request.body ), ( err, result ) => {
+				done( err );
+				if( err )
+					console.log( `error running query ${err}` );
+
+				console.log( result.rows );
+
+				response.json( {
+					data: result.rows
+				} );
+		} );
+	});
+	console.log( 'Graph Request Completed' );
 } );
 
 app.listen( app.get( 'port' ), () => {
